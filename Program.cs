a424bdf1604dd6a0773cf9b58d5546c144f51c5e
Fpuzix <fpuzix@gmail.com>
2022-08-24 15:34:47 +0300

    solution of task 47

--- Program.cs
+++ Program.cs
@@ -1305,3 +1305,42 @@
 // ShowArray(myArray);
 // Console.WriteLine(sumNum(myArray));
 
+
+// Задача № 47: Задчайте двумерный массив размером М на Н, заполненный случайными вещественными числами.
+
+double[,] GetArray(int m, int n, int min, int max)
+{
+double[,] result = new double [m,n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        result[i,j] = new Random().NextDouble()*(max-min);
+    }
+}
+return result;
+}
+
+
+void ShowArray(double[,] inArray)
+{
+
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]:f1}  ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("input M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("input N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = GetArray(m, n, 1, 10);
+
+ShowArray(array);
+
